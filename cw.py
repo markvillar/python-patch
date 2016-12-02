3925d75eefc3f164a43f0337257aa3302ad6fd24
Mark Villar <mark@markvillar.com>
2016-12-02 08:33:21 +0000

    Final commit before submission
    •Code cleanup
    •Loop number removed - no longer relevant
    •Added some comments for quick reference and improve code readability

--- cw.py
+++ cw.py
@@ -9,15 +9,16 @@ from graphics import *
 # Circle Patch
 def circlePatch(window, xOrigin, yOrigin, colour):
     
-    #Rectangular Box
+    #Square Box
     rect = Rectangle(Point(xOrigin, yOrigin), Point(xOrigin + 100, yOrigin + 100))
     rect.draw(window)    
     
+    #Circle will start small in the middle and lower part of the square
     xOrigin = xOrigin + 50
     yOrigin = yOrigin + 90
-    
     radius = 5
     
+    #Create circles
     for i in range(10):
         circle = Circle(Point(xOrigin, yOrigin), radius)
         circle.draw(window)
@@ -25,7 +26,7 @@ def circlePatch(window, xOrigin, yOrigin, colour):
         radius = radius + 5
         yOrigin = yOrigin - 5
 
-# Penultimate patch square shade
+# Penultimate patch's square drawer
 def drawSquare(window, topLeftX, topLeftY, bottomRightX, bottomRightY, colour):
     rect = Rectangle(Point(topLeftX, topLeftY), Point(bottomRightX, bottomRightY))
     rect.setFill(colour)
@@ -37,15 +38,15 @@ def penultimatePatch(window, xOrigin, yOrigin, colour):
     #Inverted Background
     drawSquare(window, xOrigin, yOrigin, xOrigin + 100, yOrigin + 100, colour)
     
-    #White Square Coodinates
+    #White Square top left coodinates
     whiteSquareXCoordinates = list(range(xOrigin + 5, xOrigin + 86, 20))
     whiteSquareYCoordinates = list(range(yOrigin, yOrigin + 101, 20))
     
-    #White Rectangular Coordinates
+    #White rectangular shape top left coordinates
     whiteRectangularXCoordinates = list(range(xOrigin, xOrigin + 101, 20))
     whiteRectangularYCoordinates = list(range(yOrigin + 10, yOrigin + 91, 20))
     
-    #Penultimate Patch Master Loop
+    #Penultimate patch master loop
     for y in range(yOrigin, yOrigin + 100, 10):
         for x in range(xOrigin, xOrigin + 100, 5):
             
@@ -57,6 +58,7 @@ def penultimatePatch(window, xOrigin, yOrigin, colour):
             if x in whiteRectangularXCoordinates and y in whiteRectangularYCoordinates:
                 drawSquare(window, x, y, x + 20, y + 10, "white")
             
+#Gets window size from user
 def getWindowSize():
     size = 0
     validSize = [5, 7, 9]
@@ -66,40 +68,34 @@ def getWindowSize():
     winSize = size * 100
     return winSize, size
     
+#Validate and assign only valid colours that are on the list
 def colourValidation(firstColour, secondColour):
     
+    #List of valid colours
     colourList = ["red", "green", "blue", "orange", "brown", "pink"]
     
+    #Keep prompting until colour is on the list and is unique
     while (secondColour == firstColour and secondColour not in colourList) or secondColour not in colourList:
         secondColour = input("Please enter another colour:")
     else:
         return secondColour
-    
+
+#Get 3 colours from user
 def getColour():
     firstColour = colourValidation("white", input("Enter the first colour:"))
-    
-    secondColour = colourValidation(firstColour, input("Enter the second colour:"))
-    
-    thirdColour = colourValidation(secondColour, input("Enter the third colour:"))
-    
+    secondColour = colourValidation(firstColour, input("Enter the second colour:"))    
+    thirdColour = colourValidation(secondColour, input("Enter the third colour:"))    
     thirdColour = colourValidation(firstColour, thirdColour)
     
     return firstColour, secondColour, thirdColour
     
-def checkSize(size):
-    if size == 5:
-        loop = 5
-    elif size == 7:
-        loop = 7
-    elif size == 9:
-        loop = 9
-    return loop
-
+#Round down X and Y coodinates to their nearest hundredth
 def getCoordinates(xPointer, yPointer):
     xPointer = 100 * math.floor(xPointer/100.0)
     yPointer = 100 * math.floor(yPointer/100.0)
     return xPointer, yPointer
-        
+
+#Main program
 def main():
     winSize, size = getWindowSize()
     win = GraphWin("Window", winSize, winSize)
@@ -107,9 +103,6 @@ def main():
     #Get choice of colour
     colourList = []
     colourList = getColour()
-    
-    #Number of loops dependant on winSize
-    loop = checkSize(size)
     
     stepper = 100
     
@@ -159,15 +152,15 @@ def main():
         #Diagonal Circle Patches
         if xOrigin == yOrigin:
             circlePatch(win, xOrigin, yOrigin, colourList[index])
-        
+            
         #Horizontal Circle Patches
         if xOrigin != 0 and yOrigin == 0:
             circlePatch(win, xOrigin, yOrigin, colourList[index])
-
+            
         #Vertical Circle Patches
         if xOrigin == (winSize - 100) and yOrigin != (winSize - 100):
             circlePatch(win, xOrigin, yOrigin, colourList[index])
-
+        
         #Penultimate Patches
         if xOrigin != (winSize - 100) and xOrigin != yOrigin and yOrigin != 0:
             
@@ -180,7 +173,7 @@ def main():
                 colour = colourList[index]
             
             penultimatePatch(win, xOrigin, yOrigin, colour)
-                
+            
         stepper = stepper + 100
         index = index + 1
         
