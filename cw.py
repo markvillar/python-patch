394b3b74065f8b38600766909b7040e959025419
Mark Villar <mark@markvillar.com>
2016-12-08 18:33:15 +0000

    Space comments
    LOL

--- cw.py
+++ cw.py
@@ -9,16 +9,16 @@ from graphics import *
 # Circle Patch
 def circlePatch(window, xOrigin, yOrigin, colour):
     
-    #Square Box
+    # Square Box
     rect = Rectangle(Point(xOrigin, yOrigin), Point(xOrigin + 100, yOrigin + 100))
     rect.draw(window)
     
-    #Circle will start small in the middle and lower part of the square
+    # Circle will start small in the middle and lower part of the square
     xOrigin = xOrigin + 50
     yOrigin = yOrigin + 95
     radius = 5
     
-    #Create circles
+    # Create circles
     for i in range(10):
         circle = Circle(Point(xOrigin, yOrigin), radius)
         circle.draw(window)
@@ -35,30 +35,30 @@ def drawSquare(window, topLeftX, topLeftY, bottomRightX, bottomRightY, colour):
 # Penultimate Patch
 def penultimatePatch(window, xOrigin, yOrigin, colour):
     
-    #Inverted Background
+    # Inverted Background
     drawSquare(window, xOrigin, yOrigin, xOrigin + 100, yOrigin + 100, colour)
     
-    #White Square top left coodinates
+    # White Square top left coodinates
     whiteSquareXCoordinates = list(range(xOrigin + 5, xOrigin + 86, 20))
     whiteSquareYCoordinates = list(range(yOrigin, yOrigin + 101, 20))
     
-    #White rectangular shape top left coordinates
+    # White rectangular shape top left coordinates
     whiteRectangularXCoordinates = list(range(xOrigin, xOrigin + 101, 20))
     whiteRectangularYCoordinates = list(range(yOrigin + 10, yOrigin + 91, 20))
     
-    #Penultimate patch master loop
+    # Penultimate patch master loop
     for y in range(yOrigin, yOrigin + 100, 10):
         for x in range(xOrigin, xOrigin + 100, 5):
             
-            #White Squares
+            # White Squares
             if x in whiteSquareXCoordinates and y in whiteSquareYCoordinates:
                 drawSquare(window, x, y, x + 10, y + 10, "white")
             
-            #White Rectangles
+            # White Rectangles
             if x in whiteRectangularXCoordinates and y in whiteRectangularYCoordinates:
                 drawSquare(window, x, y, x + 20, y + 10, "white")
             
-#Gets window size from user
+# Gets window size from user
 def getWindowSize():
     size = 0
     validSize = [5, 7, 9]
@@ -70,14 +70,14 @@ def getWindowSize():
     winSize = size * 100
     return winSize, size, winSizeOffset, offset
     
-#Get 3 colours from user
+# Get 3 colours from user
 def getColour():
     
     firstColour = ""
     secondColour = ""
     thirdColour = ""
     
-    #Valid colours' list
+    # Valid colours' list
     colourList = ["red", "green", "blue", "orange", "brown", "pink"]
     
     firstColour = input("Enter the first colour:")
